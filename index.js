e9b8d15b3471af243ac29f502c46a243326297a8
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-10-23 11:10:04 +0100

    Added backend with whitelist

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const fs = require('node:fs')
 const mime = require('mime-types')
 const sass = require('sass');
+const conf = JSON.parse(fs.readFileSync(".vyper", 'utf8'))
 const express = require('express')
 const app = express()
 const port = 80
+
+app.get("/vyper", (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if(conf.vyperadminwhitelist == ip){
+        res.send("You're in the Vyper backend")
+    }else{
+        res.sens("/")
+    }
+})
 
 app.get("*", (req, res) => {
     path = `${__dirname}/htdocs${evaluatePathString(req.path)}`
